0042eeccf2194649afc55a7b90872f37e716b123
linkenneth <linkenneth@linkenneth-macbookair.roam.corp.google.com>
2015-10-06 23:42:28 -0700

    some comments about company-mode and ycmd

--- config/completions-config.el
+++ config/completions-config.el
@@ -1,14 +1,28 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; AutoComplete Mode Config ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Company and YCMD Config ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'company-ycmd)
-(set-variable 'ycmd-server-command '("python" "/usr/lib/ycmd/ycmd/__main__.py"))
+;; Company-mode (complete-any mode) is a middle-ware / frontend / a
+;; few backends for autocompletion. Using it gives clean
+;; autocompletion for a variety of modes. Most importantly, YCMD
+;; (YouCompleteMeDaemon) is a server for autocompletion that works
+;; with any language, with built-in semantic parsing for C++, Java,
+;; Python, and Go. Company-mode supplies the frontend for YCMD
+;; completion capabilities, and while company-mode completion by
+;; itself is strong already, combined with YCMD makes it a full-scale
+;; autocompletion machine.
 
-;; Only need one character and 0 seconds (instant) to begin completion
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1)
+;; If on OS X (corp or personal laptop). This setup requires a
+;; specific installation of ycmd in a location I had determined.
+(if (string-equal system-type "darwin")
+    (progn
+      (require 'company-ycmd)
+      (set-variable 'ycmd-server-command '("python" "/usr/lib/ycmd/ycmd/__main__.py"))
 
-(company-ycmd-setup)
-(add-hook 'after-init-hook #'global-ycmd-mode)
-(add-hook 'after-init-hook #'global-company-mode)
+      ;; Only need one character and 0 seconds (instant) to begin completion
+      (setq company-idle-delay 0)
+      (setq company-minimum-prefix-length 1)
+
+      (company-ycmd-setup)
+      (add-hook 'after-init-hook #'global-ycmd-mode)
+      (add-hook 'after-init-hook #'global-company-mode)))
