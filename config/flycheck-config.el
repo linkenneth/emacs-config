0584318d7db57e1f176b0aa020db2cabde036183
Kenneth Lin <linkenneth@google.com>
2015-10-07 16:28:17 -0700

    changed package-user-dir to something in .emacs.d to remove problems

--- config/flycheck-config.el
+++ config/flycheck-config.el
@@ -4,4 +4,8 @@
 
 ;; TODO has problems with flycheck's elisp checker. Also, consider
 ;; using package manager sometime in the future?
-(setq package-user-dir nil)
+(setq flycheck-emacs-lisp-initialize-packages nil)
+(setq flycheck-emacs-lisp-package-user-dir nil)
+
+;; TODO figure out if this has problems
+(setq package-user-dir "~/.emacs.d/elpa")
