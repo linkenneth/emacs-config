19cd76c632fe9e9686dc5c7c672291b3b307b071
Kenneth Lin <linkenneth@google.com>
2015-11-20 20:35:31 -0800

    made comments ;; in miscellaneous-config and added fill-column to be 79

--- config/miscellaneous-config.el
+++ config/miscellaneous-config.el
@@ -2,66 +2,69 @@
 ;; Miscellaneous Config ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Make emacs work with emacsclient and server files
+;; Make emacs work with emacsclient and server files
 (server-start)
 
-; Some basic string manipulation, for rest of the ocnfig
+;; Some basic string manipulation, for rest of the ocnfig
 (require 's)
 
-; My name and email
+;; My name and email
 (setq user-full-name "Kenneth Lin"
       user-mail-address "linkenneth@live.com")
 
-; Set title to be name of document
+;; Set title to be name of document
 (setq frame-title-format '("Emacs @ " system-name ": %b %+%+ %f"))
 
-; Icomplete mode - autocompletion in the minibuffer
+;; Icomplete mode - autocompletion in the minibuffer
 (icomplete-mode t)
 
-; Disable tool bars
+;; Disable tool bars
 (tool-bar-mode -1)
 
-; Disable scroll bars
+;; Disable scroll bars
 (scroll-bar-mode -1)
 
-; Global line number mode
+;; Global line number mode
 (global-linum-mode t)
 
-; Inhibit startup screen
+;; Inhibit startup screen
 (setq inhibit-startup-screen t)
 
-; Undo-tree mode
+;; Undo-tree mode
 (require 'undo-tree)
 (undo-tree-mode 1)
 
-; Scrolling context lines
+;; Scrolling context lines
 (setq next-screen-context-lines 5)
 
-; Make emacs use clipboard - cross-program copying
+;; Make emacs use clipboard - cross-program copying
 (setq x-select-enable-clipboard t)
 
-; Automatically reloads buffer on change in file
+;; Automatically reloads buffer on change in file
 (global-auto-revert-mode t)
 
-; Don't ask for killing server buffers and processes (like shells)
+;; Don't ask for killing server buffers and processes (like shells)
 (remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
 (remove-hook 'kill-buffer-query-functions 'process-kill-buffer-query-function)
 
-; Disable version control through emacs (DO IT THROUGH COMMAND LINE!) (much much faster)
+;; Disable version control through emacs (DO IT THROUGH COMMAND LINE!) (much much faster)
 (setq vc-handled-backends ())
 
-; Indent spaces BY DEFAULT
+;; Indent spaces BY DEFAULT
 (setq-default indent-tabs-mode nil)
 
-; Automatically insert ')' when '(' is inserted
+;; Automatically insert ')' when '(' is inserted
 (electric-pair-mode)
 
-; Automatically indents current line and previous line when RET is hit
+;; Automatically indents current line and previous line when RET is hit
 (electric-indent-mode)
 
-; Uniquify same-name files
+;; Uniquify same-name files
 (toggle-uniquify-buffer-names)
 
-; If two files d/a1/b/c and d/a2/b/c, give filenames c|a1\b and c|a2\b
+;; If two files d/a1/b/c and d/a2/b/c, give filenames c|a1\b and c|a2\b
 (setq uniquify-buffer-name-style 'post-forward)
 (setq uniquify-strip-common-suffix t)
+
+;; Set fill-column to be almost 80 characters
+(setq-default fill-column 79)
