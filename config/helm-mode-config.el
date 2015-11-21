ad982f312576e1047bc4289ab778ff18f769c23e
Kenneth Lin <linkenneth@google.com>
2015-11-21 09:44:46 -0800

    attempt to add ido-mode-like advice

--- config/helm-mode-config.el
+++ config/helm-mode-config.el
@@ -4,9 +4,33 @@
 
 (require 'helm)
 (require 'helm-config)
+
+(helm-mode 1)
+
 (helm-autoresize-mode t)
 
+;; Fuzzy matching (like flex-matching in IDO)
 (setq helm-M-x-fuzzy-match t
       helm-buffers-fuzzy-matching t
       helm-recentf-fuzzy-match t)
-(helm-mode 1)
+
+(add-to-list 'helm-boring-file-regexp-list "\\.$")
+(add-to-list 'helm-boring-file-regexp-list "\\..$")
+
+;; Behave a bit like IDO mode:
+;; -- RET on directories continues search of it
+;; -- backspace on beginning of directory goes back a whole directory
+
+;; (defadvice helm-execute-persistent-action
+;;   (around fu/helm-find-files-navigate-forward first (&rest args) activate)
+;;   (if (file-directory-p (helm-get-selection))
+;;       (apply helm-execute-persistent-action args)
+;;     (helm-maybe-exit-minibuffer)))
+;; ;; (advice-add 'helm-execute-persistent-action :around #'fu/helm-find-files-navigate-forward)
+;; (define-key helm-find-files-map (kbd "<return>") 'helm-execute-persistent-action)
+
+;; (defun fu/helm-find-files-navigate-back (orig-fun &rest args)
+;;   (if (= (length helm-pattern) (length (helm-find-files-initial-input)))
+;;       (helm-find-files-up-one-level 1)
+;;     (apply orig-fun args)))
+;; (advice-add 'helm-ff-delete-char-backward :around #'fu/helm-find-files-navigate-back)
