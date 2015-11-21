ee0950e80cba1c0af91fa3eede1e32934acd9584
Kenneth Lin <linkenneth@google.com>
2015-11-21 10:16:52 -0800

    fixed advice for helm mode

--- config/helm-mode-config.el
+++ config/helm-mode-config.el
@@ -14,23 +14,23 @@
       helm-buffers-fuzzy-matching t
       helm-recentf-fuzzy-match t)
 
-(add-to-list 'helm-boring-file-regexp-list "\\.$")
-(add-to-list 'helm-boring-file-regexp-list "\\..$")
-
 ;; Behave a bit like IDO mode:
 ;; -- RET on directories continues search of it
 ;; -- backspace on beginning of directory goes back a whole directory
 
-;; (defadvice helm-execute-persistent-action
-;;   (around fu/helm-find-files-navigate-forward first (&rest args) activate)
-;;   (if (file-directory-p (helm-get-selection))
-;;       (apply helm-execute-persistent-action args)
-;;     (helm-maybe-exit-minibuffer)))
-;; ;; (advice-add 'helm-execute-persistent-action :around #'fu/helm-find-files-navigate-forward)
-;; (define-key helm-find-files-map (kbd "<return>") 'helm-execute-persistent-action)
+(setq original-helm-execute-persistent-action
+      (symbol-function 'helm-execute-persistent-action))
+(defadvice helm-execute-persistent-action
+  (around helm-find-files-navigate-forward first (&rest args) activate)
+  (if (file-directory-p (helm-get-selection))
+      (apply original-helm-execute-persistent-action args)
+    (helm-maybe-exit-minibuffer)))
+(define-key helm-map (kbd "<return>") 'helm-execute-persistent-action)
 
-;; (defun fu/helm-find-files-navigate-back (orig-fun &rest args)
-;;   (if (= (length helm-pattern) (length (helm-find-files-initial-input)))
-;;       (helm-find-files-up-one-level 1)
-;;     (apply orig-fun args)))
-;; (advice-add 'helm-ff-delete-char-backward :around #'fu/helm-find-files-navigate-back)
+(setq original-helm-ff-delete-char-backward
+      (symbol-function 'helm-ff-delete-char-backward))
+(defadvice helm-ff-delete-char-backward
+    (around helm-ff-delete-char-backward first (&rest args) activate)
+  (if (= (length helm-pattern) (length (helm-find-files-initial-input)))
+      (helm-find-files-up-one-level 1)
+    (apply original-helm-ff-delete-char-backward args)))
